9cb01330f82e38832232692fcaa9e10f24ba4157
Emmanuel Giron <115903418+Manny-Giron@users.noreply.github.com>
2025-01-14 18:35:01 -0500

    Initial start-up

---
+++ reactApp/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+    return (
+        <main>
+            <div className="NavContainer">
+                <div className="Men">Men</div>
+                <div className="Women">Women</div>
+                <div className="Sale">Sale</div>
+                <div className="Search-Container">
+                    <input className="Search-Input" />
+                    <div className="Search-Button">
+                    </div>
+                </div>
+                <div className="Cart">Cart</div>
+            </div>
+        </main>
+    );
+};
+
+
+export default Navbar
---
+++ reactApp/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from "../components/Navbar";
+
+
+const Home = () => {
+    return (
+        <main>
+            <Navbar />
+            <h1>test</h1>
+        </main>
+    )
+};
+
+
+export default Home;
