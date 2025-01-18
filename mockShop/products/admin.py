c14cc8f3d8604171034301a881feda1043c84d40
Emmanuel Giron <115903418+Manny-Giron@users.noreply.github.com>
2025-01-18 17:37:50 -0500

    Allow viewing / modifying product models through django buit-in admin

--- mockShop/products/admin.py
+++ mockShop/products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Products, Category, Photos  # Import your models
 
-# Register your models here.
+# Shows how 'Products' are displayed in the admin
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price') 
+    list_filter = ('categories',)          
+    search_fields = ('name',)              
+    ordering = ('id',)                     
+# Shows how 'Categorys' are displayed in the admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')          
+    search_fields = ('name',)              
+    
+# Shows how 'Photos' are displayed in the admin
+class PhotosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'image_url')  
+    list_filter = ('product',)                     
+    search_fields = ('product__name',)            
+
+# Register the models with their respective customizations
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Photos, PhotosAdmin)
