0991ed8d2a0dc0beefe6838bf264da2691f68d44
Emmanuel Giron <115903418+Manny-Giron@users.noreply.github.com>
2025-01-20 22:33:26 -0500

    Added create product endpoint

--- mockShop/products/urls.py
+++ mockShop/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductsListView, ProductDetailView
+from .views import ProductsListView, ProductDetailView, ProductCreateView
 
 urlpatterns = [
     path('products/', ProductsListView.as_view(), name="products-list"),
-    path('products/<int:pk>', ProductDetailView.as_view(), name="product")
+    path('products/<int:pk>', ProductDetailView.as_view(), name="product"),
+    path('products/create/', ProductCreateView.as_view(), name='product-create'),
+
 ]
