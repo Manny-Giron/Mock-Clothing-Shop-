baba094594933bca821ea8c36cfc962daae85279
Emmanuel Giron <115903418+Manny-Giron@users.noreply.github.com>
2025-02-14 14:45:16 -0500

    Added Product Page Route

--- mockShop/reactApp/src/App.jsx
+++ mockShop/reactApp/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AdminDashboard from "./pages/AdminDashboard";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product/:productID' element={<ProductPage />} />
         <Route path='/admin-dash' element={<AdminDashboard />} />
       </Routes>
     </Router>
