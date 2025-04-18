d8379b750d7f351f2ec97e81224258622f73ffe1
Emmanuel <emmanuel@cloudadventures.net>
2025-04-17 22:19:06 -0400

    Fixed categories creation / deletion

--- mockShop/products/views.py
+++ mockShop/products/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Products
-from .serializers import ProductsSerializer
+from .serializers import ProductsSerializer, CategorySerializer
 from .models import Products, Category, Photos
 from decimal import Decimal
 
@@ -101,10 +101,43 @@ class ProductUpdateView(APIView):
             return Response({"error" : "Serializer not valid"}, status=status.HTTP_404_NOT_FOUND)
         
 class ProductRemoveView(APIView):
-    def put(self, request, pk):
+    def delete(self, request, pk):
         try:
             product = Products.objects.get(pk=pk)
             product.delete()
             return Response({"Successfully deleted product"}, status=status.HTTP_200_OK)
         except:
             return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+class CategoriesDetailView(APIView):
+    def get(self, request):
+        try:
+            categories = Category.objects.all()
+            cats = CategorySerializer(categories, many=True)
+            return Response(cats.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"Error" : "Categories couldn't get obtained"}, status=status.HTTP_404_NOT_FOUND)
+class AddCategory(APIView):
+    def post(self, request):
+        try:
+            print("API CALL: Attempting to create category")
+            data = request.data
+            category = data
+            if category.get("name").strip() == '':
+                print("Category name is required: White space or nothing is not valid.")
+                return Response({"error": "Category name is required"}, status=status.HTTP_400_BAD_REQUEST)
+            if Category.objects.filter(name=category).exists():
+                print(f"Category {category} already exists")
+            else:
+                category = Category.objects.create(name=data["name"])
+                    
+            return Response({"id": category.id, "name": category.name}, status=status.HTTP_201_CREATED)
+        except:
+            return Response({"error": "Category could not created"}, status=status.HTTP_400_BAD_REQUEST)
+class RemoveCategory(APIView):
+    def delete(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+            category.delete()
+            return Response({"Successfully deleted category"}, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
